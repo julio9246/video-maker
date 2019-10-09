3bd6d8754aeff700726445d9f8b7efaacaadf783
Julio Cesar <julio9246@hotmail.com>
2019-10-09 17:58:27 -0300

    commit pra ir embora

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const readlinne = require('readline-sync')
+
 function start(){
     // 
     const content = {} // APESAR DE SER "CONST" O JS DEIXA A GENTE ALTERAR ESSA CONST
@@ -5,8 +7,24 @@ function start(){
     content.searchTerm = askAndReturnSearchTerm(); // quando executar essa função "askAndReturnSearchTerm"
                                                    // ele colocar o "TERM EXAMPLE" como valor dentro da searchTerm
 
+    content.prefix     = askAndReturnPrefix();
+
     function askAndReturnSearchTerm(){
-        return "TERM EXAMPLE" // testar no terminal como node index.js
+        // return "TERM EXAMPLE" // testar no terminal como node index.js
+        return readlinne.question('Type a Wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix(){
+        
+         const prefixes = ['Who is', 'What is', 'The history of'];// ARRAY DE DADOS
+
+         const selectedPrefixIndex = readlinne.keyInSelect(prefixes , 'Choose a option: '); // USA ESSA OPÇÃO PARA DAR UMA LISTA DE OPÇÃO PARA OS USUÁRIOS
+         const selectedPrefixText = prefixes[selectedPrefixIndex]; // TRAZ O TEXTO DE ACORDO COM O ÍNDICE
+         
+        // return texto ao console.log( content )
+        return selectedPrefixText
+
+
     }
 
     console.log( content );
