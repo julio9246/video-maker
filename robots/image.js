420fa0b248975dc318801a58f7e21a67158a9b3d
Julio Cesar <julio9246@hotmail.com>
2019-10-24 09:00:00 -0300

    comitando_imagens_convertidas

--- robots/image.js
+++ robots/image.js
@@ -2,6 +2,7 @@ const google = require('googleapis').google
 const imageDownloader = require('image-downloader')
 const customSearch = google.customsearch('v1') // essa é a instância responsável pelo CustomSearch
 const state = require('./state.js')
+const gm = require('gm').subClass({imageMagick: true})
 
 const googleSearchCredentials = require('../credentials/google-search.json')
 
@@ -10,11 +11,12 @@ async function robot() {
 
     const content = state.load();
     
-    await fetchImagesOfAllSentences(content)
+    // await fetchImagesOfAllSentences(content)
+    // await downloadAllImages(content)
 
-    await downloadAllImages(content)
+    await convertAllImages(content)
 
-    state.save(content)
+    // state.save(content)
 
     async function fetchImagesOfAllSentences(content) {
         // content.sentences.map((item)=>{
@@ -60,9 +62,8 @@ async function robot() {
                 try { 
                     if (content.downloadedImages.includes(imageUrl)){ // aqui eu pergunto se dentro do array já contem uma imagem baixada
                         throw new Error('Imagem já foi baixada')
-                        
                     }
-                    await downloadAndSave( imageUrl, `${sentenceIndex}-original.png`) // aqui a gente quer salvar como para cada imagem tem um index
+                    await downloadAndSave( imageUrl, sentenceIndex + '-original.png') // aqui a gente quer salvar como para cada imagem tem um index
                     content.downloadedImages.push(imageUrl)
                     console.log (`[${sentenceIndex}][${imageIndex}] > Baixou imagem com sucesso: ${imageUrl}` )
                     break 
@@ -80,7 +81,55 @@ async function robot() {
     async function downloadAndSave( url, fileName ){ // função da biblioteca image-downloader que baixa a image
         return imageDownloader.image({
             url,url,
-            dest: `./images_downloaded/ ${fileName}` 
+            dest: `./images_downloaded/${fileName}` 
+        })
+    }
+
+    async function convertAllImages( content ){
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++){
+            
+            await convertImage( sentenceIndex )
+        }
+
+    }
+
+    async function convertImage( sentenceIndex ){
+
+        return new Promise(( resolve, reject) => {
+            const inputFile = `./images_downloaded/${sentenceIndex}-original.png[0]`
+            const outputFile = `./images_downloaded/${sentenceIndex}-converted.png`
+            const width = 1920
+            const height = 1080
+
+            gm()
+            .in(inputFile) 
+            .out("(")
+              .out("-clone")
+              .out("0")
+              .out("-background", "white")
+              .out("-blur", "0x9")
+              .out("-resize", `${width}x${height}^`)
+            .out(")")
+            .out("(")
+              .out("-clone")
+              .out("0")
+              .out("-background", "white")
+              .out("-resize", `${width}x${height}`)
+            .out(")")
+            .out("-delete", "0")
+            .out("-gravity", "center")
+            .out("-compose", "over")
+            .out("-composite")
+            .out("-extent", `${width}x${height}`)
+            .write(outputFile, (error) => {
+              if (error) {
+                return reject(error)
+              }
+    
+              console.log(`> [video-robot] Image converted: ${outputFile}`)
+              resolve()
+            })
+    
         })
     }
  
