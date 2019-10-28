2a8c55331484100f859b6f9652eb091152eadec0
Julio Cesar <julio9246@hotmail.com>
2019-10-28 15:18:10 -0300

    'subindo_corretivo_video'

--- robots/image.js
+++ robots/image.js
@@ -1,27 +1,24 @@
+
+const imageDownloader = require('image-downloader') 
 const google = require('googleapis').google
-const imageDownloader = require('image-downloader')
 const customSearch = google.customsearch('v1') // essa é a instância responsável pelo CustomSearch
 const state = require('./state.js')
-const gm = require('gm').subClass({imageMagick: true})
 
 const googleSearchCredentials = require('../credentials/google-search.json')
-
 
 async function robot() {
 
     const content = state.load();
+     
     
     await fetchImagesOfAllSentences(content)
-    await downloadAllImages(content)
-
-    await convertAllImages(content)
+    await downloadAllImages(content) 
 
     state.save(content)
 
-    async function fetchImagesOfAllSentences(content) {
-        // content.sentences.map((item)=>{
-        //     console.log("item -> ", item );
-        // });
+
+
+    async function fetchImagesOfAllSentences(content) { 
 
         for (const sentence of content.sentences) { // AQUI ESTOU DANDO UM LOOP DENTRO DE TODAS AS SENTENÇAS
             const query = `${content.searchTerm} ${sentence.keywords[0]}` // AQUI EU CRIO UMA STRING QUE CONCATENA O MEU SEARCH TERM COM A PRIMEIRA KEYWORD
@@ -30,16 +27,17 @@ async function robot() {
         }
     }
 
-    async function fetchGoogleAndReturnImageLinks(query) {
-
+    async function fetchGoogleAndReturnImageLinks(query) { 
+        
         const response = await customSearch.cse.list({
             auth: googleSearchCredentials.apiKey, // api para logar
             cx: googleSearchCredentials.searchEngineId, // cx ( provavelmente context ) para definir ao CSE qual pesquisa utilizar
             q: query,// aqui eu passo o "query" para procurar a imagem
-            num: 2, // o numero de imagens que irá retornar
-            searchType: 'image'
+            searchType: 'image',
+            num: 2 // o numero de imagens que irá retornar
 
         })
+         
 
         const imagesUrl = response.data.items.map((item) => {
             return item.link
@@ -50,14 +48,23 @@ async function robot() {
 
     }
 
+
     async function downloadAllImages(content) {
+
+
         content.downloadedImages = [] // array das minhas imagens baixadas
  
         for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
             const images = content.sentences[sentenceIndex].images
+            
+            
+            console.log( content.sentences[0].images );
+            
 
             for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
                 const imageUrl = images[imageIndex]
+
+                
 
                 try { 
                     if (content.downloadedImages.includes(imageUrl)){ // aqui eu pergunto se dentro do array já contem uma imagem baixada
@@ -79,59 +86,14 @@ async function robot() {
     } 
 
     async function downloadAndSave( url, fileName ){ // função da biblioteca image-downloader que baixa a image
+        
         return imageDownloader.image({
             url,url,
             dest: `./images_downloaded/${fileName}` 
         })
     }
 
-    async function convertAllImages( content ){
-        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++){
-            
-            await convertImage( sentenceIndex )
-        }
-
-    }
-
-    async function convertImage( sentenceIndex ){
-
-        return new Promise(( resolve, reject) => {
-            const inputFile = `./images_downloaded/${sentenceIndex}-original.png[0]`
-            const outputFile = `./images_downloaded/${sentenceIndex}-converted.png`
-            const width = 1920
-            const height = 1080
-
-            gm()
-            .in(inputFile)  
-            .out("(")
-              .out("-clone")
-              .out("0")
-              .out("-background", "white")
-              .out("-blur", "0x9")
-              .out("-resize", `${width}x${height}^`)
-            .out(")")
-            .out("(")
-              .out("-clone")
-              .out("0")
-              .out("-background", "white")
-              .out("-resize", `${width}x${height}`)
-            .out(")")
-            .out("-delete", "0")
-            .out("-gravity", "center")
-            .out("-compose", "over")
-            .out("-composite")
-            .out("-extent", `${width}x${height}`)
-            .write(outputFile, (error) => {
-              if (error) {
-                return reject(error)
-              }
     
-              console.log(`> [video-robot] Image converted: ${outputFile}`)
-              resolve()
-            })
-    
-        })
-    }
  
 }
 
